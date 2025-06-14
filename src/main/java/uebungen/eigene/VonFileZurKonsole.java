108748afcbe09df3544d8a589fb1bed369dc1566
irmakozfe <irmakdamla.oezdemir@study.thws.de>
2025-06-14 16:41:55 +0200

    new exercises are done

---
+++ src/main/java/uebungen/eigene/VonFileZurKonsole.java
@@ -0,0 +1,24 @@
+package uebungen.eigene;
+
+import java.io.*;
+
+public class VonFileZurKonsole {
+    public static void main(String[] args) throws IOException {
+        // try-with-resources
+
+        try(
+                FileInputStream fis= new FileInputStream("/home/damla/IdeaProjects/Prog2/src/main/java/uebungen/eigene/input.txt");
+                InputStreamReader isr= new InputStreamReader(fis);
+                BufferedReader br = new BufferedReader(isr);
+                ){
+            String line;
+            while ((line = br.readLine()) != null) { // read() : -1, readLine() : null
+                System.out.println(line);
+            }
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
